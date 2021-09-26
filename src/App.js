f9282d07aa3dba5b7b2cbbdf5c016ab343662df2
Merve Erbaş <merverbas40@gmail.com>
2021-09-26 17:59:13 +0300

    Upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React from 'react';
+import Dashboard from './Dashboard';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+ 
+function App(){
+ 
+        return (
+            <div>
+                <Navbar/>
+                <div class="container-fluid" id="main">
+                 <div class="row row-offcanvas row-offcanvas-left">
+                   <Sidebar/>
+                  <Dashboard/>
+                
+             </div>
+            </div>  
+        </div>  
+        );
+    }
+  
+export default App
